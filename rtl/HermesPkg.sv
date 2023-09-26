b4850a7cc2273b8b33e8a958e33e6344831485c9
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2023-09-26 16:48:48 -0300

    Add package

---
+++ rtl/HermesPkg.sv
@@ -0,0 +1,25 @@
+/**
+ * Hermes
+ * @file HermesPkg.sv
+ * 
+ * @author Angelo Elias Dal Zotto (angelo.dalzotto@edu.pucrs.br)
+ * GAPH - Hardware Design Support Group (https://corfu.pucrs.br/)
+ * PUCRS - Pontifical Catholic University of Rio Grande do Sul (http://pucrs.br/)
+ * 
+ * @date September 2023
+ * 
+ * @brief SystemVerilog Hermes package.
+ */
+
+package HermesPkg;
+
+    localparam NPORT = 5;
+    typedef enum logic [$clog2(NPORT) - 1 : 0] {
+		HERMES_EAST,
+		HERMES_WEST,
+		HERMES_NORTH,
+		HERMES_SOUTH,
+		HERMES_LOCAL
+    } hermes_port_t;
+
+endpackage
