0d9283e8cb10f53436c333d7119e48176b45cd03
Willian Analdo Nunes (Aluno IC - Ney) <willian.nunes@edu.pucrs.br>
2023-10-02 17:56:50 -0300

    Added Trafic generator

---
+++ sim/inputs/traffic_generator.py
@@ -0,0 +1,29 @@
+import random as rm
+import math
+
+##### Uniform Distribution #####
+size      = 32    # Packet Size
+bandwidth = 32    # Bandwidth
+rate      = 1     # Rate 
+amount    = 1     # Amount of Packets
+x_total   = 4     # X Noc Size
+y_total   = 4     # Y Noc Size
+
+def xy():
+    while True:
+        x = math.floor(rm.uniform( 0 , x_total ))
+        y = math.floor(rm.uniform( 0 , y_total ))
+        if str(x)+str(y) != router:
+            break
+    return(x,y)
+
+for i in range(math.floor(x_total)):
+    for j in range(math.floor(y_total)):
+
+        router = str(i) + str(j)
+        t = 1024 
+        with open(str(router)+".txt", "w") as output:
+            for k in range(amount):
+                x, y = xy()
+                output.write(str(t) + '\t' + str(x) + '\t' +  str(y) + '\t' + str(size))
+                t += int((bandwidth * size)/ rate)
