c4050138fbb883b521c17f3fc85665f4b09f5713
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2024-05-16 14:47:52 -0300

    Change payload size control to EOP instead of counter-based

--- rtl/HermesBuffer.sv
+++ rtl/HermesBuffer.sv
@@ -28,41 +28,51 @@ module HermesBuffer
     input  logic clk_i,
     input  logic rst_ni,
 
-    input  logic rx_i,
-    input  logic req_ack_i,
-    input  logic data_ack_i,
+    /* Buffer input */
+    input  logic                     rx_i,
+    input  logic                     eop_i,
+    output logic                     credit_o,
     input  logic [(FLIT_SIZE - 1):0] data_i,
 
+    /* Buffer output */
+    output logic                     data_av_o,
+    output logic                     eop_o,
+    input  logic                     data_ack_i,
+    output logic [(FLIT_SIZE - 1):0] data_o,
+    
+    /* Routing request control */
     output logic req_o,
-    output logic credit_o,
-    output logic data_av_o,
-    output logic sending_o,
-    output logic [(FLIT_SIZE - 1):0] data_o
+    input  logic req_ack_i,
+    output logic sending_o
 );
 
-    logic tx;
+    logic               tx;
+    logic               eop;
+    logic [FLIT_SIZE:0] data; /* 1 bit extra for EOP */
+
+    assign data_o = data[(FLIT_SIZE-1):0];
+    assign eop    = data[FLIT_SIZE];
+    assign eop_o  = eop;
 
     RingBuffer #(
-        .DATA_SIZE(32),
-        .BUFFER_SIZE(BUFFER_SIZE)
+        .DATA_SIZE  (FLIT_SIZE + 1), /* 1 bit extra for EOP */
+        .BUFFER_SIZE(BUFFER_SIZE  )
     ) ringbuffer (
-        .clk_i      (clk_i),
-        .rst_ni     (rst_ni),
-        .rx_i       (rx_i),
-        .rx_ack_o   (credit_o),
-        .data_i     (data_i),
-        .tx_o       (tx),
-        .tx_ack_i   (data_ack_i),
-        .data_o     (data_o)
+        .clk_i   (clk_i          ),
+        .rst_ni  (rst_ni         ),
+        .rx_i    (rx_i           ),
+        .rx_ack_o(credit_o       ),
+        .data_i  ({eop_i, data_i}),
+        .tx_o    (tx             ),
+        .tx_ack_i(data_ack_i     ),
+        .data_o  (data           )
     );
 
     /* FSM Control */
-    typedef enum logic [4:0] {
-        SEND_INIT    = 5'b00001,
-        SEND_REQ     = 5'b00010,
-        SEND_HEADER  = 5'b00100,
-        SEND_SIZE    = 5'b01000,
-        SEND_PAYLOAD = 5'b10000
+    typedef enum logic [2:0] {
+        SEND_INIT    = 3'b001,
+        SEND_REQ     = 3'b010,
+        SEND_PAYLOAD = 3'b100
     } fsm_t;
 
     fsm_t state;
@@ -75,34 +85,14 @@ module HermesBuffer
             state <= next_state;
     end
 
-    /* Flit counter control */
-    logic [(FLIT_SIZE - 1):0] flit_cntr;
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-        if (!rst_ni) begin
-            flit_cntr <= '0; 
-        end
-        else begin
-            case (state)
-                SEND_SIZE:    flit_cntr <= data_o;
-                SEND_PAYLOAD: begin
-                    if (data_ack_i && tx)
-                        flit_cntr <= flit_cntr - 1'b1;
-                end
-                default:      flit_cntr <= '0;
-            endcase
-        end
-    end
-
     always_comb begin
         case (state)
             SEND_INIT:    next_state = tx         ? SEND_REQ     : SEND_INIT;
-            SEND_REQ:     next_state = req_ack_i  ? SEND_HEADER  : SEND_REQ;
-            SEND_HEADER:  next_state = data_ack_i ? SEND_SIZE    : SEND_HEADER;
-            SEND_SIZE:    next_state = data_ack_i ? SEND_PAYLOAD : SEND_SIZE;
-            SEND_PAYLOAD: next_state = (data_ack_i && tx && flit_cntr == 32'b1)
+            SEND_REQ:     next_state = req_ack_i  ? SEND_PAYLOAD : SEND_REQ;
+            SEND_PAYLOAD: next_state = (data_ack_i && tx && eop)
                                                                  ? SEND_INIT
                                                                  : SEND_PAYLOAD;
-            default:      next_state =                             SEND_INIT;
+            default:      next_state = SEND_INIT;
         endcase
     end
 
@@ -111,9 +101,9 @@ module HermesBuffer
     assign req_o = (state == SEND_REQ);
 
     /* Active */
-    assign sending_o = (state inside {SEND_HEADER, SEND_SIZE, SEND_PAYLOAD});
+    assign sending_o = (state == SEND_PAYLOAD);
 
     /* Data request control */
-    assign data_av_o = (state inside {SEND_HEADER, SEND_SIZE, SEND_PAYLOAD} && tx);
+    assign data_av_o = (state == SEND_PAYLOAD && tx);
 
 endmodule
